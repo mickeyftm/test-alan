a4b0783a01436ddb79da2aade111e12cc6e5e85e
Polyshark <polybear.finance@gmail.com>
2021-05-09 16:48:02 +0200

    fix: timer block height

--- src/views/Home/components/Timer.tsx
+++ src/views/Home/components/Timer.tsx
@@ -44,7 +44,7 @@ const Timer = () => {
 
 
   const stratTime = Date.now() / 1000; // use UNIX timestamp in seconds
-  const endTime = 1620579600; // use UNIX timestamp in seconds
+  const endTime = 1620686040; // use UNIX timestamp in seconds
 
   const remainingTime = endTime - stratTime;
   const days = Math.ceil(remainingTime / daySeconds);
