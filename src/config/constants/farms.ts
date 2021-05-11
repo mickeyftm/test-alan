e5c93cc5a9b229e74a49aa9165c990e680668812
Polyshark <polybear.finance@gmail.com>
2021-05-11 15:36:17 +0200

    fix: quick fix

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -29,8 +29,8 @@ const farms: FarmConfig[] = [
     tokenAddresses: {
       137: '0xD201B8511aaB3E9b094b35ABcD5d7863c78D6d0e',
     },
-    quoteTokenSymbol: QuoteToken.USDC,
-    quoteTokenAdresses: contracts.usdc,
+    quoteTokenSymbol: QuoteToken.PDOGE,
+    quoteTokenAdresses: contracts.pdoge,
   },
   // {
   //   pid: 1,
@@ -95,22 +95,22 @@ const farms: FarmConfig[] = [
     quoteTokenSymbol: QuoteToken.USDC,
     quoteTokenAdresses: contracts.usdc,
   },
-  {
-    pid: 10,
-    risk: 5,
-    isTokenOnly: true,
-    lpSymbol: 'PDOGE',
-    decimal: 18,
-    lpAddresses: {
-      137: '0x264e6bc3f95633725658e4d9640f7f7d9100f6ac'
-    },
-    tokenSymbol: 'PDOGE',
-    tokenAddresses: {
-      137: '0x8a953cfe442c5e8855cc6c61b1293fa648bae472'
-    },
-    quoteTokenSymbol: QuoteToken.WMATIC,
-    quoteTokenAdresses: contracts.wmatic,
-  },
+  // {
+  //   pid: 10,
+  //   risk: 5,
+  //   isTokenOnly: true,
+  //   lpSymbol: 'PDOGE',
+  //   decimal: 18,
+  //   lpAddresses: {
+  //     137: '0x264e6bc3f95633725658e4d9640f7f7d9100f6ac'
+  //   },
+  //   tokenSymbol: 'PDOGE',
+  //   tokenAddresses: {
+  //     137: '0x8a953cfe442c5e8855cc6c61b1293fa648bae472'
+  //   },
+  //   quoteTokenSymbol: QuoteToken.WMATIC,
+  //   quoteTokenAdresses: contracts.wmatic,
+  // },
   {
     pid: 1,
     risk: 3,
